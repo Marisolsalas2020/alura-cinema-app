2854a69beab2cb7bbf859895fa25568df309e137
Marisol Salas <129517176+Marisolsalas2020@users.noreply.github.com>
2024-07-14 12:46:14 -0300

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import Favoritos from "pages/Favoritos";
+import NotFound from "pages/NotFound";
+import PaginaBase from "pages/PaginaBase";
+import Player from "pages/Player";
+
+const { default: Inicio } = require("pages/Inicio");
+const { BrowserRouter, Routes, Route } = require("react-router-dom");
+
+function AppRoutes() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PaginaBase/>}>
+                    <Route index element={<Inicio/>}></Route>
+                    <Route path="favoritos" element={<Favoritos/>}></Route>
+                    <Route path=":id" element={<Player/>}></Route>
+                    <Route path="*" element={<NotFound/>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default AppRoutes
